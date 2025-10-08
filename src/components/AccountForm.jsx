50f1a64be95b86e2b0fb71488b9feab6e9511598
Steven Wanderski <steven.wanderski@gmail.com>
2025-10-08 06:21:59 -0500

    Adds react hook form

--- src/components/AccountForm.jsx
+++ src/components/AccountForm.jsx
@@ -1,37 +1,35 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { changePassword } from "@/lib/auth-client";
 import Message from "@/components/Message";
 
 export default function AccountForm({ user }) {
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [currentPassword, setCurrentPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState('');
+  const { register, handleSubmit, formState, watch, reset } = useForm();
+  const { errors } = formState;
 
-  const submit = async (event) => {
-    event.preventDefault();
+  const newPassword = watch('newPassword');
 
-    // setLoading(true);
+  const submit = async (values) => {
+    setLoading(true);
 
-    // const { data, error } = await changePassword({
-    //   newPassword: password,
-    //   currentPassword: "oldpassword1234"
-    // });
+    const { data, error } = await changePassword({
+      newPassword: values.newPassword,
+      currentPassword: values.currentPassword
+    });
 
-    // setLoading(false);
+    setLoading(false);
 
-    // if (!error) {
-    //   setMessage('Password has been updated.');
-    // } else {
-    //   setError(error);
-    // }
+    if (!error) {
+      setMessage('Password has been updated.');
+    } else {
+      setError(error);
+    }
 
     setMessage('Password has been updated.');
-
-    setCurrentPassword('');
-    setNewPassword('');
-    setConfirmPassword('');
+    setLoading(false);
+    reset();
   }
 
   const closeMessage = () => {
@@ -42,18 +40,16 @@ export default function AccountForm({ user }) {
     <div>
       {message && <Message text={message} onClose={closeMessage} />}
 
-      <form onSubmit={submit}>
+      <form onSubmit={handleSubmit(submit)}>
         <div className="space-y-6 max-w-[400px]">
           <div>
             <label htmlFor="currentPassword" className="label mb-1">Current Password</label>
             <input
-              type="text"
-              id="currentPassword"
-              name="currentPassword"
+              {...register('currentPassword', { required: 'Current Password is required' })}
+              type="password"
               className="input"
-              value={currentPassword}
-              onChange={(e) => setCurrentPassword(e.target.value)}
             />
+            {errors.currentPassword && <p className="text-red-500 text-sm mt-1">{errors.currentPassword.message}</p>}
           </div>
 
           <hr className="hr" />
@@ -61,25 +57,26 @@ export default function AccountForm({ user }) {
           <div>
             <label htmlFor="newPassword" className="label mb-1">New Password</label>
             <input
-              type="text"
-              id="newPassword"
-              name="newPassword"
+              {...register('newPassword', { required: 'New Password is required' })}
+              type="password"
               className="input"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
             />
+            {errors.newPassword && <p className="text-red-500 text-sm mt-1">{errors.newPassword.message}</p>}
           </div>
 
           <div>
             <label htmlFor="confirmPassword" className="label mb-1">Confirm Password</label>
             <input
-              type="text"
-              id="password"
-              name="confirmPassword"
+              {...register('confirmPassword', {
+                required: 'Confirm Password is required',
+                validate: (value) => {
+                  return value === newPassword || "Passwords do not match";
+                }
+              })}
+              type="password"
               className="input"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
             />
+            {errors.confirmPassword && <p className="text-red-500 text-sm mt-1">{errors.confirmPassword.message}</p>}
           </div>
 
           <div>

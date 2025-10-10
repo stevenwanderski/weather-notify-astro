5ae3609d27a662bb0e4139897f689ce897d5811d
Steven Wanderski <steven.wanderski@gmail.com>
2025-10-10 13:54:51 -0500

    Adds styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { navigate } from "astro:transitions/client";
 import { signOut } from "@/lib/auth-client";
+import cloudy from '@/assets/cloudy.png';
 
-export default function Navbar({ email }) {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar({ email, pathname }) {
+  const navigation = [
+    { name: 'Dashboard', href: '/dashboard', current: pathname == '/dashboard' },
+    { name: 'Account', href: '/account', current: pathname == '/account' }
+  ]
+
   const logout = () => {
     signOut({
       fetchOptions: {
@@ -13,20 +25,93 @@ export default function Navbar({ email }) {
   }
 
   return (
-    <div className="bg-zinc-900">
-      <div className="container mx-auto py-4">
-        <div className="flex items-center justify-between text-white">
-          <div className="flex gap-8">
-            <a href="/dashboard" className="hover:underline">Dashboard</a>
-            <a href="/account" className="hover:underline">Account</a>
+    <Disclosure as="nav" className="relative bg-zinc-800">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <div className="shrink-0">
+              <img
+                alt="Sky Brief"
+                src={cloudy.src}
+                className="h-8 w-auto"
+              />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" */}
+                <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
+                  Dashboard
+                </a>
+                <a
+                  href="#"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"
+                >
+                  Account
+                </a>
+              </div>
+            </div>
           </div>
+          <div className="hidden sm:ml-6 sm:block">
+            <div className="flex items-center gap-4">
+              <div className="text-gray-400 text-sm font-medium">
+                {email}
+              </div>
 
-          <div className="flex items-center gap-8">
-            {email}
-            <button className="btn !bg-zinc-700" type="button" onClick={logout}>Logout</button>
+              <button
+                type="button"
+                className="cursor-pointer relative rounded-full p-1 text-sm font-medium text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+                onClick={logout}
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+          <div className="-mr-2 flex sm:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+            </DisclosureButton>
           </div>
         </div>
       </div>
-    </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" */}
+          <DisclosureButton
+            as="a"
+            href="/dashboard"
+            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+          >
+            Dashboard
+          </DisclosureButton>
+          <DisclosureButton
+            as="a"
+            href="/account"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white"
+          >
+            Account
+          </DisclosureButton>
+        </div>
+        <div className="border-t border-white/10 pt-4 pb-3">
+          <div className="flex items-center px-5">
+            <div className="text-base font-medium text-gray-400">tom@example.com</div>
+          </div>
+          <div className="mt-3 space-y-1 px-2">
+            <DisclosureButton
+              as="button"
+              href="#"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white"
+              onClick={logout}
+            >
+              Sign out
+            </DisclosureButton>
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
   )
 }

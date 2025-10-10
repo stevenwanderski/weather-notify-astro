39c0a887aa7ef3e023585ec819e0d665c7f3a687
Steven Wanderski <steven.wanderski@gmail.com>
2025-10-10 09:45:47 -0500

    Adds styled login and register pages

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,11 +3,13 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "@/lib/auth-client";
+import cloudy from '@/assets/cloudy.png';
 
 const schema = z
   .object({
     email: z.string().nonempty("Email is required"),
-    password: z.string().nonempty("Password is required")
+    password: z.string().nonempty("Password is required"),
+    rememberMe: z.boolean()
   });
 
 export default function LoginForm() {
@@ -22,6 +24,7 @@ export default function LoginForm() {
     signIn.email({
       email: values.email,
       password: values.password,
+      rememberMe: values.rememberMe,
       callbackURL: "/dashboard"
     }, {
       onRequest: (ctx) => {
@@ -37,50 +40,103 @@ export default function LoginForm() {
   }
 
   return (
-    <div>
-      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-10">
-        Login.
-      </h1>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img src={cloudy.src} alt="Logo" className="size-10 mx-auto" />
+        <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
 
-      {error && (
-        <div className="bg-red-50 text-red-500 text-sm p-4 mb-10 rounded max-w-[400px]">
-          {error}
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
+          {error && (
+            <div className="bg-red-50 text-red-500 text-sm p-4 mb-10 rounded max-w-[400px]">
+              {error}
+            </div>
+          )}
+
+          <form onSubmit={handleSubmit(submit)} className="space-y-6">
+            <div>
+              <label htmlFor="email" className="label">Email</label>
+              <input
+                {...register('email')}
+                className="input"
+              />
+              {errors.email && <p className="error-text mt-1">{errors.email.message}</p>}
+            </div>
+
+            <div>
+              <label htmlFor="password" className="label">Password</label>
+              <input
+                {...register('password')}
+                className="input"
+                type="password"
+              />
+              {errors.password && <p className="error-text mt-1">{errors.password.message}</p>}
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex gap-3">
+                <div className="flex h-6 shrink-0 items-center">
+                  <div className="group grid size-4 grid-cols-1">
+                    <input
+                      {...register('rememberMe')}
+                      id="remember-me"
+                      type="checkbox"
+                      className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-blue-500 checked:bg-blue-500 indeterminate:border-blue-600 indeterminate:bg-blue-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
+                    />
+                    <svg
+                      fill="none"
+                      viewBox="0 0 14 14"
+                      className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
+                    >
+                      <path
+                        d="M3 8L6 11L11 3.5"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="opacity-0 group-has-checked:opacity-100"
+                      />
+                      <path
+                        d="M3 7H11"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="opacity-0 group-has-indeterminate:opacity-100"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <label htmlFor="remember-me" className="block text-sm/6 text-gray-900">
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm/6">
+                <a href="/request-password" className="font-semibold text-blue-500 hover:text-blue-600">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="cursor-pointer flex w-full justify-center rounded-md bg-yellow-400 px-3 py-1.5 text-sm/6 font-semibold text-black shadow-xs hover:bg-yellow-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
         </div>
-      )}
 
-      <div className="mb-10">
-        <form className="max-w-[400px]" onSubmit={handleSubmit(submit)}>
-          <div className="mb-4">
-            <label htmlFor="email" className="label">Email</label>
-            <input
-              {...register('email')}
-              className="input"
-            />
-            {errors.email && <p className="error-text mt-1">{errors.email.message}</p>}
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="password" className="label">Password</label>
-            <input
-              {...register('password')}
-              className="input"
-              type="password"
-            />
-            {errors.password && <p className="error-text mt-1">{errors.password.message}</p>}
-          </div>
-
-          <div className="flex justify-between items-center mb-6">
-            <button className="btn" disabled={loading}>Submit</button>
-
-            <a href="/register" className="hover:underline text-sm">Create an account</a>
-          </div>
-
-          <hr className="hr" />
-
-          <div className="flex justify-start items-center">
-            <a href="/request-password" className="hover:underline text-sm">Forgot password?</a>
-          </div>
-        </form>
+        <p className="mt-10 text-center text-sm/6 text-gray-500">
+          Not a member?{' '}
+          <a href="/register" className="font-semibold text-blue-500 hover:text-blue-600">
+            Click here to create an account.
+          </a>
+        </p>
       </div>
     </div>
   )
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,6 +4,7 @@ import { navigate } from 'astro:transitions/client';
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import cloudy from '@/assets/cloudy.png';
 
 const schema = z
   .object({
@@ -43,44 +44,59 @@ export default function RegisterForm() {
   }
 
   return (
-    <div>
-      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-10">
-        Register.
-      </h1>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img src={cloudy.src} alt="Logo" className="size-10 mx-auto" />
+        <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Create an account
+        </h2>
+      </div>
 
-      {error && (
-        <div className="bg-red-50 text-red-500 text-sm p-4 mb-10 rounded max-w-[400px]">
-          {error}
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
+          {error && (
+            <div className="bg-red-50 text-red-500 text-sm p-4 mb-10 rounded max-w-[400px]">
+              {error}
+            </div>
+          )}
+
+          <form onSubmit={handleSubmit(submit)} className="space-y-6">
+            <div>
+              <label htmlFor="email" className="label">Email</label>
+              <input
+                {...register('email')}
+                className="input"
+              />
+              {errors.email && <p className="error-text mt-1">{errors.email.message}</p>}
+            </div>
+
+            <div>
+              <label htmlFor="password" className="label">Password</label>
+              <input
+                {...register('password')}
+                className="input"
+                type="password"
+              />
+              {errors.password && <p className="error-text mt-1">{errors.password.message}</p>}
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="cursor-pointer flex w-full justify-center rounded-md bg-yellow-400 px-3 py-1.5 text-sm/6 font-semibold text-black shadow-xs hover:bg-yellow-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+              >
+                Create account
+              </button>
+            </div>
+          </form>
         </div>
-      )}
 
-      <div className="mb-10">
-        <form className="max-w-[400px]" onSubmit={handleSubmit(submit)}>
-          <div className="mb-4">
-            <label htmlFor="email" className="label">Email</label>
-            <input
-              {...register('email')}
-              className="input"
-            />
-            {errors.email && <p className="error-text mt-1">{errors.email.message}</p>}
-          </div>
-
-          <div className="mb-4">
-            <label htmlFor="password" className="label">Password</label>
-            <input
-              {...register('password')}
-              className="input"
-              type="password"
-            />
-            {errors.password && <p className="error-text mt-1">{errors.password.message}</p>}
-          </div>
-
-          <div className="flex justify-between items-center">
-            <button className="btn" disabled={loading}>Submit</button>
-
-            <a href="/login" className="hover:underline text-sm">Login</a>
-          </div>
-        </form>
+        <p className="mt-10 text-center text-sm/6 text-gray-500">
+          Have an account?{' '}
+          <a href="/login" className="font-semibold text-blue-500 hover:text-blue-600">
+            Click here to sign in.
+          </a>
+        </p>
       </div>
     </div>
   )
